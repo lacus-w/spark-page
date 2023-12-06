56be2e0f53445565be063acd4be4434353912b25
lacus w <cheio@outlook.com>
2023-12-06 11:53:42 +0000

    fix conf

--- setup.sh
+++ setup.sh
@@ -7,16 +7,13 @@ python -m pip install --upgrade --no-cache-dir pip setuptools
 python -m pip install --upgrade --no-cache-dir sphinx readthedocs-sphinx-ext
 python -m pip install --exists-action=w --no-cache-dir -r docs/requirements.txt
 cat docs/source/conf.py
-python -m sphinx -T -E -b html -d _build/doctrees -D language=en . $READTHEDOCS_OUTPUT/html
+python -m sphinx -T -E -b html -d _build/doctrees -D language=en . ~/html
 
-# rst to myst
-pip install "rst-to-myst[sphinx]"
-rst2myst convert docs/**/*.rst
-rst2myst convert --config myst.yaml docs/**/*.rst
 
 # md to myst
-python -m pip install myst_parser
-pip install myst-parser
+pip install myst-parser sphinx-design sphinx-copybutton
 
 pip install sphinx-book-theme
 pip install nbsphinx
+
+brew install python@3.10
