c4c205d4ac2a44754a40acd4304e427fe88719f7
lacus w <cheio@outlook.com>
2023-12-06 13:01:39 +0000

    fix favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,8 +34,8 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
-# html_theme = "sphinx_book_theme"
+# html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
 # html_theme = sphinx_material
 
 # -- Options for EPUB output
