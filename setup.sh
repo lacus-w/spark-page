6dd220cfb9d9345fae8502744caa0be5c0cc8739
lacus w <cheio@outlook.com>
2023-12-06 11:24:40 +0000

    fix conf

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 git clone --depth 1 https://github.com/lacus-w/spark-page.git .
 git fetch origin --force --prune --prune-tags --depth 50
 cat .readthedocs.yaml
-asdf global python 3.8.18
+asdf global python 3.10.13
 python -mvirtualenv $READTHEDOCS_VIRTUALENV_PATH
 python -m pip install --upgrade --no-cache-dir pip setuptools
 python -m pip install --upgrade --no-cache-dir sphinx readthedocs-sphinx-ext
-python -m pip install --upgrade --upgrade-strategy only-if-needed --no-cache-dir .
+python -m pip install --exists-action=w --no-cache-dir -r docs/requirements.txt
+cat docs/source/conf.py
+python -m sphinx -T -E -b html -d _build/doctrees -D language=en . $READTHEDOCS_OUTPUT/html
 
 # rst to myst
 pip install "rst-to-myst[sphinx]"
@@ -17,3 +19,4 @@ python -m pip install myst_parser
 pip install myst-parser
 
 pip install sphinx-book-theme
+pip install nbsphinx
